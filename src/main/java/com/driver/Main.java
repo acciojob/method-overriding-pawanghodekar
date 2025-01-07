059a980d572d5b4777dae6c3cce469c8376a4223
pavan <pawan.fbypa19@sinhgad.edu>
2025-01-07 12:36:43 +0530

    Overide

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -23,16 +23,12 @@ public class Main {
 
 */
 
-// Task 1: Create class A
 class A {
     public String meth() {
         return "Invoking method from class A";
     }
 }
-
-// Task 2: Create class B extending class A
 class B extends A {
-    // Task 4: Override method meth in class B
     @Override
     public String meth() {
         return "Method is overridden in Extended class B";
@@ -41,13 +37,8 @@ class B extends A {
 
 public class Main {
     public static void main(String[] args) {
-        // Task 3: Create object of class B and call method meth from class A
         A objA = new A();
-        System.out.println(objA.meth());
-
-        // Create object of class B and call method meth
         B objB = new B();
-        System.out.println(objB.meth());
     }
 }
 
