929156866d067e1079e78a4b039dc87e872c3282
pavan <pawan.fbypa19@sinhgad.edu>
2025-01-07 11:26:02 +0530

    Overide

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        B obj = new B();
+        // Call method from class A
+        System.out.println(obj.meth());
 
+    }
 }
 
