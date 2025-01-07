8ef30b2c99d501994d2d1f0ae7b64d80df0f990a
pavan <pawan.fbypa19@sinhgad.edu>
2025-01-07 13:26:11 +0530

    Overide

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,20 @@
 package com.driver;
-
-import com.driver.A;
-import com.driver.B;
-
 public class Main {
-    public static void main(String[] args) {
-        A objA = new A();
-        System.out.println(objA.meth());
-
-        B objB = new B();
-        System.out.println(objB.meth());
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
     }
+    public  static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.meth();
+
+    }
+
 }
