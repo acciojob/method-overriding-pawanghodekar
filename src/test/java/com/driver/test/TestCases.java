23ddd8ce947fa0686dc6fe44d9e06760b7a0c1f9
pavan <pawan.fbypa19@sinhgad.edu>
2025-01-07 13:38:44 +0530

    Overide

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,7 +1,26 @@
 package com.driver.test;
+
+import com.driver.Main;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestCases {
+
+    @Test
+    public void testprintHelloWorld2() {
+        Main.B obj = new Main.B();
+        String result = obj.meth();
+        assertEquals("Method is overridden in Extended class B", result);
+    }
+}
+
+
+/*
+package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
     
 }
+*/
